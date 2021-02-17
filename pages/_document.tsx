2f91406928b612f55c346e11146a9725fd4aa339
EminDeniz99 <emindeniz99@gmail.com>
2021-02-17 22:56:35 +0300

    delete images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
 import { IconContext } from 'react-icons'
+import React from 'react'
 
 export default class MyDocument extends Document {
   render() {
@@ -8,33 +9,8 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.png' />
+            <link rel='shortcut icon' href={process.env.NEXT_PUBLIC_favicon} />
 
-            <link
-              rel='apple-touch-icon'
-              sizes='180x180'
-              href='/apple-touch-icon.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='96x96'
-              href='/favicon-96x96.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='/favicon-32x32.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='16x16'
-              href='/favicon-16x16.png'
-            />
-
-            <link rel='manifest' href='/manifest.json' />
           </Head>
 
           <body>
